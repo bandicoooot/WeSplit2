46c8e4457ec4a84feb122a6cfe68a1d687574160
bandicoooot <mahdilashgari98@gmail.com>
2026-06-16 00:43:36 +0200

    finish

--- WeSplit2/ContentView.swift
+++ WeSplit2/ContentView.swift
@@ -8,32 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
-    let priate = ["Luffy","ZORO","God Usopp"]
-    
-    @State private var yonkons = "Luffy"
+    @State private var checkAmount = 0.0
+    @State private var NumberOfVolsk = 2
+    @State private var Tipppercentage = 20
+    @FocusState private var amountIsFocused: Bool
+    let Tipppercentages = [10, 15, 20, 25, 0]
+    var totalPerPerson: Double{
+        //calculate
+        let pepoleCount = Double(NumberOfVolsk + 2)
+        let TippWählen = Double(Tipppercentage)
+        
+        let tippKalkulation = checkAmount / 100 * TippWählen
+        let total = checkAmount + tippKalkulation
+        let anteil = total / pepoleCount
+        
+        return anteil
+    }
     var body: some View {
         NavigationStack{
             Form{
-//                //            TextField("gib mir dein name", text: $name)
-//                //            Text("Hello Pirate \(name)")
-//    //            ForEach(0..<1000) {number in
-//    //                Text("Row \(number)")
-//                ForEach(0..<1000) {
-//                    Text("Row \($0)")
-                Picker("Select Your favorite Pirate", selection: $yonkons){
-                    ForEach(priate, id: \.self){
-                        Text($0)
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("number of pepole", selection: $NumberOfVolsk) {
+                        ForEach(2..<100) {
+                            Text("\($0) pepole")
+                        }
                     }
+                    .pickerStyle(.navigationLink)
                 }
-                    
+                Section("wie viel würdest du Tippen?"){
+                    Picker("Tipp percentages", selection: $Tipppercentage){
+                        ForEach(Tipppercentages, id: \.self) {
+                            Text($0, format: .percent)
+                            
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                Section{
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency? .identifier ?? "USD"))
                 }
             }
-        .navigationTitle("chosse your team")
-            
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
         }
 
     }
-
+ 
 
 #Preview {
     ContentView()
